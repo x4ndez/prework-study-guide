6d1227f39a7b50fcc62427869867c126a5a2d605
x.ande <ananfro@live.com>
2023-03-15 13:26:10 +1100

    Add JS component to webpage

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// --- 0.0 : VARIABLE DEFINITIONS
+
+var topic = "HTML"; // CALLED 0.1
+
+var topics = ["HTML", "CSS", "Git", "JavaScript"]; // CALLED 0.3
+
+var shapes = ["triangle", "square", "pentagon", "circle"]; // CALLED 0.2
+
+// --- 0.3 : VARIABLE DEFINITIONS : FOR LOOP EXAMPLE : Print to console all shapes in array 'topics'.
+
+for(var x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+
+}
+
+/*
+
+// --- 0.1 : IF STATEMENT EXAMPLE : Print to console response associated with value of 'topic'.
+
+if (topic === 'HTML') {
+
+  console.log("Let's study HTML!");
+
+} else if (topic === 'CSS') {
+
+  console.log("Let's study CSS!");
+
+} else if (topic === 'Git') {
+
+  console.log("Let's study Git!");
+
+} else if (topic === 'JavaScript') {
+
+  console.log("Let's study JavaScript!");
+
+} else {
+
+  console.log('Please try again!');
+
+}
+
+// --- 0.2 : FOR LOOP EXAMPLE : Print to console all shapes in array 'shapes'.
+
+for (var i = 0; i < shapes.length; i++) {
+
+    console.log(shapes[i]);
+
+}
+
+*/
